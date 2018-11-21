f0a475e79fdce2e921beb384cdf0705862c09d26
Adriel <aponte96.la@gmail.com>
2018-11-20 23:41:11 -0300

    Working with npm and browserify and lodash

--- myjs.js
+++ myjs.js
@@ -1,3 +1,11 @@
+var _ = require("lodash");
+
+console.log(_);
+
+var array = [1,2,3,4,5,6,7,8,9,0];
+
+console.log(_.without(array, 3));
+
 var h6 = document.querySelector("h6");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
